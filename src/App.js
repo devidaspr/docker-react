415c7ea0693e01fbf2a7392c6655b3c4c83e7ca5
devidas <devidasp@hotmail.com>
2020-08-07 22:13:01 -0700

    changed app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          11111223334445556667778899
+          Deploying on AWS
         </p>
         <a
           className="App-link"
